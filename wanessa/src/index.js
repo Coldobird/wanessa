91863fa8503d1e0a90d3b9c38ec6a170dea2651a
Coldobird <twuestefeld@gmail.com>
2025-04-02 19:57:59 -0300

    wip

--- wanessa/src/index.js
+++ wanessa/src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -11,32 +11,9 @@ import SocialProof from './SocialProof.js';
 import About from './About.js';
 import Footer from './Footer.js';
 
-// const ScrollChecker = () => {
-//   useEffect(() => {
-//     const scrollables = [...document.querySelectorAll('*')].filter(el => {
-//       const style = window.getComputedStyle(el);
-//       const isScrollable = (
-//         (el.scrollHeight > el.clientHeight || el.scrollWidth > el.clientWidth) &&
-//         (style.overflow === 'auto' || style.overflow === 'scroll' || style.overflowY === 'auto' || style.overflowY === 'scroll') &&
-//         style.display !== 'inline'
-//       );
-//       return isScrollable;
-//     });
-
-//     scrollables.forEach(el => {
-//       el.style.outline = '2px solid red';
-//       console.log('Actually scrollable element:', el);
-//     });
-//   }, []);
-
-//   return null;
-// };
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ScrollChecker /> */}
     <Header />
     <Hero />
     <Treatments />
